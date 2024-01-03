f62a03a68c42af64b6cce6fdca84d64a0707232a
Krupal Virani <krupalvirani@gmail.com>
2024-01-03 22:59:05 +0530

    hosting orbitControllCheck

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://threejs.org/build/three.module.js"
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.120.0/examples/jsm/controls/OrbitControls.js";
 
 //Scene
 const scene = new THREE.Scene()
