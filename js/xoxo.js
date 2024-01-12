14272ac7fa1ea75f181d8b1051fa3d31f5c51327
Krupal Virani <krupalvirani@gmail.com>
2024-01-12 23:16:44 +0530

    XO rectified

--- js/xoxo.js
+++ js/xoxo.js
@@ -9,7 +9,7 @@ scene.background = new THREE.Color(0x111111); // Background color
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 1;
+camera.position.set(2,1,2);
 
 // Renderer
 const canvas = document.querySelector('.webgl');
